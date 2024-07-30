fdfe59abe54c228e98c057cdfe9e9b88446cfbec
ducmai <ducmai4203@gmail.com>
2024-07-31 01:32:52 +0700

    login function with jwt

--- src/main/java/springboot/studentcourse/Application.java
+++ src/main/java/springboot/studentcourse/Application.java
@@ -1,13 +1,34 @@
 package springboot.studentcourse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import springboot.studentcourse.user.User;
+import springboot.studentcourse.user.UserRepository;
 
 @SpringBootApplication
-public class Application {
+public class Application{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+//
+//	@Autowired
+//	UserRepository userRepository;
+//	@Autowired
+//	PasswordEncoder passwordEncoder;
+
+//	@Override
+//	public void run(String... args) throws Exception {
+//		// Khi chương trình chạy
+//		// Insert vào csdl một user.
+//		User user = new User();
+//		user.setUsername("loda");
+//		user.setPassword(passwordEncoder.encode("loda"));
+//		userRepository.save(user);
+//		System.out.println(user);
+//	}
 
 }
--- src/main/java/springboot/studentcourse/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package springboot.studentcourse.service;
-
-import springboot.studentcourse.dto.UserDto;
-import springboot.studentcourse.entity.User;
-
-public interface UserService {
-    User findByUsername(String username);
-    User save(UserDto userDto);
-}
